e4d3c70aec37bff2e70db41461f491ee571c7c52
Bùi Phú Khuyên <khuyenpb@gmail.com>
2019-05-14 10:34:00 +0700

    no message

---
+++ lib/src/resources/ProfileSetting.dart
@@ -0,0 +1,16 @@
+
+import 'package:english_app/src/resources/widgets/TopBar.dart';
+import 'package:flutter/material.dart';
+
+class ProfileSetting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          TopBar()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/resources/widgets/Colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Color firstColor = Color(0xFF2196F3);
+Color secondColor = Color(0xFF42A5F5).withOpacity(0.5);
+Color thirdColor = Color(0xFF64B5F6).withOpacity(0.2);
+
+Color greyColor = Color(0xFFE6E6E6);
+Color darkGreyColor = Color(0xFFB8B2CB);
+
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.blue,
+);
