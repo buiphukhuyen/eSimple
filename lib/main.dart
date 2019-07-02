ef2b51f53376a4a3891ef6d1c2163a017b341f2f
buiphukhuyen <khuyenpb@gmail.com>
2019-07-02 22:19:06 +0700

    Update UI Finished Voca (Speech)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:english_app/src/resources/Homepage/Vocabulary/FinishVoca.dart';
 import 'package:english_app/src/resources/User/PageLogin.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: PageLogin(),
+      home: PageLogin(), //FinishVoca(),
       debugShowCheckedModeBanner: false, //Hide Debug Mode in Simulator
     ));
--- lib/src/resources/Homepage/Vocabulary/FinishVoca.dart
+++ lib/src/resources/Homepage/Vocabulary/FinishVoca.dart
@@ -3,6 +3,9 @@ import 'package:english_app/src/resources/widgets/TopBar.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class FinishVoca extends StatelessWidget {
+  final String topic;
+  FinishVoca({this.topic});
+
   Widget _buildBottomCard(double width, double height) {
     return Container(
       width: width,
@@ -64,17 +67,221 @@ class FinishVoca extends StatelessWidget {
             Align(
                 alignment: Alignment.bottomCenter,
                 child: _buildBottomCard(width, height)),
-            Container(
-              padding: EdgeInsets.all(200.0),
-              child: CircularPercentIndicator(
-                radius: 210.0,
-                lineWidth: 10.0,
-                animation: true,
-                percent: 0.1,
-                center: Text('RUNNING'),
-                circularStrokeCap: CircularStrokeCap.round,
-                progressColor: Colors.red,
-              ),
+            Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 200.0, right: 90.0, left: 90.0),
+                  //color: Colors.blue,
+                  child: CircularPercentIndicator(
+                      radius: 210.0,
+                      lineWidth: 10.0,
+                      animation: true,
+                      percent: 0.1,
+                      center: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'TỪ VỰNG',
+                            style: TextStyle(
+                                fontSize: 24.0,
+                                letterSpacing: 1.5,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 4.0,
+                          ),
+                          Container(
+                            height: 5.0,
+                            width: 80.0,
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '10%',
+                            style: TextStyle(
+                                fontSize: 40.0, fontWeight: FontWeight.bold),
+                          ),
+                          Center(
+                            child: Text(
+                              'CÁC HOẠT ĐỘNG THƯỜNG NGÀY',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.blue,
+                                  letterSpacing: 1.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                      progressColor: Colors.red,
+                      backgroundColor: Colors.grey[200]),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 100.0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        width: 170,
+                        height: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black12, blurRadius: 15.0),
+                              BoxShadow(color: Colors.black12, blurRadius: 10.0)
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(
+                              'SỐ TỪ VỰNG\nTRONG CHỦ ĐỀ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  '10',
+                                  style: TextStyle(
+                                    fontSize: 35.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '/10',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 170,
+                        height: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black12, blurRadius: 15.0),
+                              BoxShadow(color: Colors.black12, blurRadius: 10.0)
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(
+                              'TỔNG TỪ VỰNG\nĐÃ HỌC',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  '10',
+                                  style: TextStyle(
+                                    fontSize: 35.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '/100',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text('CHỦ ĐỀ TIẾP THEO',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            letterSpacing: 1.0)),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("ontap");
+                      },
+                      child: Container(
+                        width: 250,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.blue[500],
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black12, blurRadius: 15.0),
+                              BoxShadow(color: Colors.black12, blurRadius: 10.0)
+                            ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Image.asset("assets/images/Voca/Voca_Country.png",
+                                width: 60),
+                            Text(
+                              "CÁC QUỐC GIA",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
             Align(
                 alignment: Alignment.bottomCenter,
